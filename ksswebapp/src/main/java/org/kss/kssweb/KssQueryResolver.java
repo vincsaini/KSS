67e64eda0e00e6b3493c83d3aa8da8e89b9da3ed
vineet <vineet@192.168.0.5>
2018-06-24 12:33:34 +0530

    Integrated with UI

--- ksswebapp/src/main/java/org/kss/kssweb/KssQueryResolver.java
+++ ksswebapp/src/main/java/org/kss/kssweb/KssQueryResolver.java
@@ -3,17 +3,18 @@ package org.kss.kssweb;
 import org.kss.pojo.QueryEntityMapper;
 import org.kss.services.KSSService;
 import org.kss.services.KSSServiceFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 public class KssQueryResolver {
 
 	@RequestMapping("/querykss")
 	public String resolveQuery(
 			@RequestParam(value = "query", required = true, defaultValue = "price Carrort Surat") String query) {
+		String resultJson="";
 		try {
 			// get the entities present in the query
 			// make a call to entity resolver to get the entities
@@ -28,7 +29,7 @@ public class KssQueryResolver {
 			System.out.println("Queries and entities returned " + queryEntityMapper.toString());
 			//
 			KSSService kssService = KSSServiceFactory.getKSSService(queryEntityMapper.getIntent().getName());
-			String resultJson = kssService.serveRequest(queryEntityMapper);
+			resultJson = kssService.serveRequest(queryEntityMapper);
 			System.out.println("Result from the service"+resultJson);
 			
 		} catch (Exception e) {
@@ -36,6 +37,6 @@ public class KssQueryResolver {
 			e.printStackTrace();
 			return "error";
 		}
-		return "greeting";
+		return resultJson;
 	}
 }
