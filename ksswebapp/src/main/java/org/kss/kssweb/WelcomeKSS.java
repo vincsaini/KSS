8a4a4adb60bf6deb5f11782d193af1d19af532e2
vineet <vineet@192.168.0.5>
2018-07-10 22:29:08 +0530

    Merging the UI updates.

--- ksswebapp/src/main/java/org/kss/kssweb/WelcomeKSS.java
+++ ksswebapp/src/main/java/org/kss/kssweb/WelcomeKSS.java
@@ -11,7 +11,8 @@ public class WelcomeKSS {
     @GetMapping("/kss")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "welcomekss";
+        //return "welcomekss";
+        return "home";
     }
 
 }
